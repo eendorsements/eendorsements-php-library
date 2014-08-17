8dd5dea7f564825f4c2418fe885117d589f4c96d
Justin Dalrymple <wholehogsoftware@gmail.com>
2014-08-17 18:21:10 -0500

    WIP Wrapper, Examples

---
+++ examples/eEndorsementsView.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "../eEndorsementsAPIExchange.php";
+
+$settings = array(
+	'apiKey' => 'e91b551137ea834591282ad12780261b',
+	'apiSecretKey' => '08b2af83f209b3a8d5bba2edf774b625'
+);
+
+$ee = new eEndorsementsAPIExchange($settings);
+
+$result = $ee->makeRequest('http://eendorsements.com/api/users/view');
+
+echo $result;
