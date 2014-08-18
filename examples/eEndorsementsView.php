fb20692386ea5d1b8f040f1ab7d7b1424e9bfeda
redstar504 <redstar504@gmail.com>
2014-08-18 12:29:59 -0700

    Code formatting and better query param support

--- examples/eEndorsementsView.php
+++ examples/eEndorsementsView.php
@@ -3,14 +3,16 @@
 require_once "../eEndorsementsAPIExchange.php";
 
 $settings = array(
-	'apiKey' => 'e91b551137ea834591282ad12780261b',
-	'apiSecretKey' => '08b2af83f209b3a8d5bba2edf774b625'
+    'apiKey'       => 'e91b551137ea834591282ad12780261b',
+    'apiSecretKey' => '08b2af83f209b3a8d5bba2edf774b625'
 );
 
 $ee = new eEndorsementsAPIExchange($settings);
 
 $username = "abcstaffing";
 
-$result = $ee->makeRequest('http://eendorsements.com/api/endorsements/view/' . $username);
+$ee->setGetFields(array('page' => 2));
+
+$result = $ee->makeRequest('http://eendorsements.dev/api/endorsements/view/'.$username);
 
 echo $result;
